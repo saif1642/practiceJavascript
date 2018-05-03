e4c66594ea1304f33ecfd973fdd88b6feed13c30
saifdev1642 <saifdev1642@gmail.com>
2018-05-03 19:04:47 +0600

    generate random number by a range

--- randomNumberGenerator/random.js
+++ randomNumberGenerator/random.js
@@ -1,7 +1,16 @@
 //Generate random Number beetweens zero to user range
 
-var range = prompt('Enter your range:');
+// var range = prompt('Enter your range:');
 
-var randomNumber = Math.random()*range+1;
+// var randomNumber = Math.random()*range+1;
 
-document.write("Generated Random Number is: "+randomNumber);
+function getRandom(max,min){
+   var Number = Math.floor(Math.random() * (max - min + 1)) + min;
+   return Number;
+}
+
+var minRange = prompt('Enter the minimum range');
+var maxRange = prompt('Enter the maximum range');
+var randomNumber = getRandom(maxRange,minRange);
+console.log(randomNumber);
+document.write("Generated Random Number between "+minRange +" and "+maxRange+" is: "+randomNumber);
