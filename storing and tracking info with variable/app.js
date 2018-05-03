c7e48cf6629ba9552edcee6189adf0c2b179c5e4
saifdev1642 <saifdev1642@gmail.com>
2018-05-03 16:39:05 +0600

    Storing and tracking info with variable

---
+++ storing and tracking info with variable/app.js
@@ -0,0 +1,22 @@
+// // capture visitor input
+// var visitorName=prompt('What is your name?');
+
+// //combining visitor input with string
+
+// const message ="Hello"+ visitorName+", Welcome to my site";
+
+// alert(message);
+
+
+
+
+//Making a Story board
+
+var visitorAdjective = prompt('Enter a Adjective');
+var visitorVerb = prompt('Enter a Verb');
+var visitorNoun =prompt('Enter a Noun');
+alert('you are done, Ready to see your story?');
+
+
+var story = "Once upon a time there was a "+visitorAdjective+" programmer named "+visitorNoun+" who loved to do "+visitorVerb;
+document.write(story);
